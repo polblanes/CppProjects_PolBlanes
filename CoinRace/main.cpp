faf4d0d9783f1e0b7a0a2b9d77ca0ced182d115c
polblanescebrian <polblanescebrian@S1-45>
2017-03-14 13:32:03 +0100

    structure

---
+++ CoinRace/main.cpp
@@ -0,0 +1,38 @@
+#include "coinManager.h"
+#include "map.h"
+#include "player.h"
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+
+void main() {
+	/*
+	std::cout << "." << " " << "." << " " << "." << std::endl;
+	std::cout << "." << " " << "@" << " " << "." << std::endl;
+	std::cout << "." << " " << "$" << " " << "." << std::endl;
+	*/
+
+	//DYNAMIC MATRIX
+	/*
+	int **md;
+	int rows{ 5 }, columns{ 5 };
+
+	md = new int*[rows];
+
+	for (int i = 0; i < rows; i++)
+		md[i] = new int[columns];
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			md[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			std::cout << md[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	*/
+}
